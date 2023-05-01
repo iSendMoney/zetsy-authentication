3d937fe1384bf0847432abda0277579595cc6360
sudeepmi <sudeepm0599@gmail.com>
2023-05-01 23:16:14 +0545

    ..

--- middlewares/protected.js
+++ middlewares/protected.js
@@ -2,7 +2,13 @@ const jwt = require("jsonwebtoken");
 module.exports = {
   authenticateToken: (req, res, next) => {
     const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[0];
+    let token;
+    if(authHeader && authHeader.startsWith("Bearer")){
+      token = authHeader.split(" ")[1];
+    }
+    if(authHeader){
+      token = authHeader.split(" ")[0];
+    }
     console.log(token)
     if (!token) {
       return res.status(401).send("Unauthorized");
